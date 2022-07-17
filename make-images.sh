7b29e29a0eb43b486727cd9a3153f1768872f75f
soulteary <soulteary@gmail.com>
2022-07-17 13:32:39 +0800

    chore: upgrade golang to 1.18.4

--- make-images.sh
+++ make-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t soulteary/golang-playground:web-1.18.3 -f docker/Dockerfile.web .
-docker build -t soulteary/golang-playground:sandbox-1.18.3 -f docker/Dockerfile.sandbox .
-docker build -t soulteary/golang-playground:actuator-1.18.3 -f docker/Dockerfile.actuator .
+docker build -t soulteary/golang-playground:web-1.18.4 -f docker/Dockerfile.web .
+docker build -t soulteary/golang-playground:sandbox-1.18.4 -f docker/Dockerfile.sandbox .
+docker build -t soulteary/golang-playground:actuator-1.18.4 -f docker/Dockerfile.actuator .
